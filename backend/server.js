496bc673234f9856f2d3ff71c6658eaa9a25cc22
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-04-20 00:18:18 +0200

    multiple fetch requests, and separate errors

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-/////////////////////////
+///////////////////////
 app.use(express.json()) // parse JSON request bodies
 
 app.post('/api/search', (req, res) => {
   const { postcode, houseNumber, houseLetter } = req.body
 
   const energyLabelUrl =
-    'https://public.ep-online.nl/api/v3/PandEnergieLabel/Adres?postcode=6222TD&huisnummer=2&huisletter='
+    'https://public.ep-online.nl/api/v3/PandEnergieLabel/Adres?postcode=6222TD&huisnummer=2&huisletter=i'
 
   const energyLabelConfig = {
     headers: {
@@ -26,48 +26,55 @@ app.post('/api/search', (req, res) => {
     },
   }
 
-  const requests = [axios.get(energyLabelUrl, energyLabelConfig)]
+  const bagUrl =
+    'https://api.bag.kadaster.nl/lvbag/viewerbevragingen/v2/adressen/?expand=true&postcode=6222TD&huisnummer=2&inclusiefEindStatus=true'
+
+  const bagConfig = {
+    headers: {
+      accept: 'application/hal+json',
+      'X-Api-Key': process.env.BAG_API_KEY,
+    },
+  }
+
+  const requests = [
+    axios.get(energyLabelUrl, energyLabelConfig).catch(() => ({})),
+    axios.get(bagUrl, bagConfig).catch(() => ({})),
+  ]
 
   Promise.all(requests)
     .then((responses) => {
-      const data = responses.map((response) => response.data)
-      res.json(data)
+      const errors = responses.filter(
+        (response) => !response.data || response.data.error
+      )
+      if (errors.length > 0) {
+        const errorMessages = errors.map((error) => {
+          if (error.message === 'el-error') {
+            return 'The first request failed'
+          } else if (error.message === 'bag-error') {
+            return 'The BAG request failed'
+          } else {
+            return 'Error. The address is incorrect or the house can not be found in the database.'
+          }
+        })
+        res.json({
+          errors: errorMessages,
+          data: responses
+            .filter((response) => response.data && !response.data.error)
+            .map((response) => response.data),
+        })
+      } else {
+        res.json({
+          data: responses.map((response) => response.data),
+        })
+      }
     })
     .catch((error) => {
       console.log(error)
-      res.json(
-        'Error. The address is incorrect or the house can not be found in the database.'
-      )
+      res.status(500).json({
+        errors: ['An internal server error occurred'],
+      })
     })
 })
-
-//working request code
-// app.post('/api/search', (req, res) => {
-//   const { postcode, houseNumber, houseLetter } = req.body
-
-//   const url =
-//     'https://public.ep-online.nl/api/v3/PandEnergieLabel/Adres?postcode=6222TD&huisnummer=2&huisletter='
-
-//   const config = {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Authorization: process.env.ENERGY_LABEL_API_KEY,
-//     },
-//   }
-
-//   axios
-//     .get(url, config)
-//     .then((response) => {
-//       res.json(response.data) // send the retrieved data back to the frontend as a JSON response
-//     })
-//     .catch((error) => {
-//       console.log(error)
-//       res.json(
-//         'Error. The address is incorrect or the house can not be found in the database.'
-//       )
-//     })
-// })
-////////////////////////
 
 const PORT = process.env.PORT || 7000
 
