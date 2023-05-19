912c46c9215604dfdab7752bb7beb9e6af6297ac
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-05-19 16:30:15 +0200

    responsive rent price animation

--- frontend/main.js
+++ frontend/main.js
@@ -119,16 +119,92 @@ form.addEventListener('submit', (event) => {
 
 //setting the overflow to visible when animation ends
 
-const typingElement = document.querySelector('.typing')
+const rentPrice = document.querySelector('.rent-price')
+const maxLegalPrice = document.querySelector('.max-legal-price')
 const price = document.querySelector('.price')
 
+let isSmallScreen = screen.width <= 415 ? true : false
+
 // Get the amount of charaters
-const textWidth = typingElement.innerText.length - 4
-typingElement.style.width = `${textWidth}ch`
+let textWidth
+if (!isSmallScreen) {
+  rentPrice.classList.add('typing')
+  maxLegalPrice.classList.remove('typing')
+  maxLegalPrice.classList.remove('typing-price')
 
-typingElement.addEventListener('animationend', () => {
-  typingElement.style.overflow = 'visible'
-  typingElement.style.border = 'none'
+  textWidth = rentPrice.innerText.length - 4
 
-  price.style.animation = 'hover-delay .8s forwards'
-})
+  rentPrice.style.width = `${textWidth}ch`
+  // price.style.opacity = '1 !important'
+
+  //old eventlistener
+  rentPrice.addEventListener('animationend', () => {
+    rentPrice.style.overflow = 'visible'
+    rentPrice.style.border = 'none'
+    price.style.animation = 'hover-delay .8s forwards'
+  })
+} else {
+  rentPrice.classList.remove('typing')
+  maxLegalPrice.classList.add('typing')
+  // price.classList.add('typing-price')
+  price.style.marginLeft = '200px'
+  price.style.margin = '0 auto'
+  price.style.opacity = '0'
+
+  textWidth = maxLegalPrice.innerText.length - 4
+
+  maxLegalPrice.style.width = `${textWidth}ch`
+
+  maxLegalPrice.addEventListener('animationend', () => {
+    if (!isSmallScreen) {
+      maxLegalPrice.style.overflow = 'visible'
+      maxLegalPrice.style.border = 'none'
+      price.style.animation = 'hover-delay .8s forwards'
+    } else {
+      maxLegalPrice.style.border = 'none'
+      writePrice()
+    }
+  })
+
+  function writePrice() {
+    price.classList.add('typing-price')
+
+    price.addEventListener('animationend', () => {
+      price.style.overflow = 'visible'
+      price.style.border = 'none'
+
+      price.style.animation = 'hover-delay .8s forwards'
+    })
+  }
+}
+
+// if (screen.width < 415) {
+//   isSmallScreen = true
+//
+// } else {
+//   isSmallScreen = false
+//
+// }
+// console.log(isSmallScreen)
+
+// maxLegalPrice.addEventListener('animationend', () => {
+//   if (!isSmallScreen) {
+//     maxLegalPrice.style.overflow = 'visible'
+//     maxLegalPrice.style.border = 'none'
+//     price.style.animation = 'hover-delay .8s forwards'
+//   } else {
+//     maxLegalPrice.style.border = 'none'
+//     writePrice()
+//   }
+// })
+
+// function writePrice() {
+//   price.classList.add('typing-price')
+
+//   price.addEventListener('animationend', () => {
+//     price.style.overflow = 'visible'
+//     price.style.border = 'none'
+
+//     price.style.animation = 'hover-delay .8s forwards'
+//   })
+// }
